d1cd3dc3fdca4fe6ed72c94fa2b7583bcfdc5b0a
WojtekU <voytecu@gmail.com>
2022-07-06 13:45:38 +0200

    anns css and navbar

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,8 +3,17 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer id="footer">
-        FOOTER
-    </footer>
+    <div class="foot">
+      <a href="https://www.linkedin.com/in/anne-musyoki/"><i class="fab fa-linkedin"></i></a>
+      <a href="mailto:anmomoan@gmail.com"><i class="fas fa-envelope-square"></i></a>
+      <a href="https://github.com/mueni2"><i class="fab fa-github"></i></a>
+    </div>
+    
+      <div>
+        Copyright &copy; 2022 - W2M
+      </div>
+    
+  </footer>
   )
 }
 
---
+++ client/src/components/Learn/Learn.jsx
@@ -0,0 +1,26 @@
+import './Learn.css';
+import banana from '../../assets/banana.mp4'
+
+const Learn = () => {
+  return (
+    <div>
+             
+            <h1 class="heading">
+              LEARN-SIGN-LANGUAGE
+            </h1>
+            <div class="img-container">
+                <video controls src={banana}></video> 
+            </div>
+           
+            
+            <hr/>
+          
+          <div class="text">
+            <h5>Word of the day: "banana"</h5>
+          </div>
+    
+    </div>
+  )
+}
+
+export default Learn;
