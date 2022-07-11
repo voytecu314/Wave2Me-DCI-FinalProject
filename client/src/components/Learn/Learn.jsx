ba6a7e1f258843927704f550d6e5644b837f6bd8
mueni2 <anmomoan@gmail.com>
2022-07-11 14:07:56 +0200

    search

--- client/src/components/Learn/Learn.jsx
+++ client/src/components/Learn/Learn.jsx
@@ -1,16 +1,32 @@
 import './Learn.css';
 import banana from '../../assets/banana.mp4'
+import { useState } from 'react';
 //import {useEffect} from 'react';
 
 const Learn = () => {
 
-  
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  }
+
   return (
     <div>
              
             <h1 class="heading">
               LEARN-SIGN-LANGUAGE
             </h1>
+
+            <hr/>
+            <input
+            type='text'
+            placeholder='Search here'
+            onChange={handleChange}
+            value={searchInput} />
+
+            <hr />
 
             <div class="img-container">
                 <video controls src={banana}></video> 
