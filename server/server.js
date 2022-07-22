3ef1711ff674bd310de9bc3ef7ee5ff24c27257b
WojtekU <voytecu@gmail.com>
2022-07-22 08:19:40 +0200

    before pull must commit

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-console.log(process.env);
+
 //ROUTES
 app.use('/', loginRoutes);
 app.use('/', blogRoutes);
